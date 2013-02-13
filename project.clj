e18702126323131c92541a472b6c90177644118d
Max Penet <m@qbits.cc>
2013-02-13 15:20:33 +0100

    add repo link and desc

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject cc.qbits/alia "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Cassandra CQL3 client for Clojure (datastax/java-driver wrapper)"
+  :url "https://github.com/mpenet/alia"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
