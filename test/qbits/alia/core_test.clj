062bb66ba46cb3eb4868286a16b0911c318b6360
Max Penet <m@qbits.cc>
2013-02-13 20:17:16 +0100

    no need to keep referencing *session* since we have a binding

--- test/qbits/alia/core_test.clj
+++ test/qbits/alia/core_test.clj
@@ -73,12 +73,12 @@
     ;; prepare the thing
     (binding [*cluster* (cluster "127.0.0.1" :port 9042)]
       (with-session (connect *cluster*)
-        (try (execute *session* "DROP KEYSPACE alia;")
+        (try (execute "DROP KEYSPACE alia;")
              (catch Exception _ nil))
-        (execute *session* "CREATE KEYSPACE alia
+        (execute  "CREATE KEYSPACE alia
          WITH replication = {'class': 'SimpleStrategy', 'replication_factor' : 3};")
-        (execute *session* "USE alia;")
-        (execute *session* "CREATE TABLE users (
+        (execute "USE alia;")
+        (execute "CREATE TABLE users (
                 user_name varchar,
                 first_name varchar,
                 last_name varchar,
@@ -91,63 +91,63 @@
                 amap map<varchar, bigint>,
                 PRIMARY KEY (user_name)
               );")
-        (execute *session* "CREATE INDEX ON users (birth_year);")
+        (execute "CREATE INDEX ON users (birth_year);")
 
-        (execute *session* "INSERT INTO users (user_name, first_name, last_name, emails, birth_year, amap, tags, auuid,valid)
+        (execute "INSERT INTO users (user_name, first_name, last_name, emails, birth_year, amap, tags, auuid,valid)
        VALUES('frodo', 'Frodo', 'Baggins', {'f@baggins.com', 'baggins@gmail.com'}, 1, {'foo': 1, 'bar': 2}, [4, 5, 6], '1f84b56b-5481-4ee4-8236-8a3831ee5892', true);")
-        (execute *session* "INSERT INTO users (user_name, first_name, last_name, emails, birth_year, amap, tags, auuid, valid)
+        (execute "INSERT INTO users (user_name, first_name, last_name, emails, birth_year, amap, tags, auuid, valid)
        VALUES('mpenet', 'Max', 'Penet', {'m@p.com', 'ma@pe.com'}, 0, {'foo': 1, 'bar': 2}, [1, 2, 3], '42048d2d-c135-4c18-aa3a-e38a6d3be7f1', true);")
 
         ;; do the thing
         (test-runner)
 
         ;; destroy the thing
-        (try (execute *session* "DROP KEYSPACE alia;") (catch Exception _ nil))
-        (shutdown *session*)
+        (try (execute "DROP KEYSPACE alia;") (catch Exception _ nil))
+        (shutdown)
         (shutdown *cluster*)))))
 
 (deftest test-sync-execute
   (is (= user-data-set-as-map
-         (execute->map *session* "select * from users;"))))
+         (execute->map "select * from users;"))))
 
 (deftest test-async-execute
   ;; promise
   (is (= user-data-set-as-map
-         (rows->maps @(execute *session* "select * from users;" :async? true))))
+         (rows->maps @(execute "select * from users;" :async? true))))
   ;; callback
   (let [p (promise)]
-    (execute *session* "select * from users;"
+    (execute  "select * from users;"
              :success (fn [r] (deliver p (rows->maps r))))
     (is (= user-data-set-as-map @p))))
 
 
 (deftest test-prepared
-  (let [s-simple (prepare *session* "select * from users;")
-        s-parameterized-simple (prepare *session* "select * from users where user_name=?;")
-        s-parameterized-bigint (prepare *session* "select * from users where birth_year=?;")
-        s-prepare-types (prepare *session* "INSERT INTO users (user_name, auuid, created, valid) VALUES(?, ?, ?, ?);")
-        ;; s-parameterized-set (prepare *session* "select * from users where emails=?;")
-        ;; s-parameterized-nil (prepare *session* "select * from users where session_token=?;")
+  (let [s-simple (prepare "select * from users;")
+        s-parameterized-simple (prepare "select * from users where user_name=?;")
+        s-parameterized-bigint (prepare "select * from users where birth_year=?;")
+        s-prepare-types (prepare "INSERT INTO users (user_name, auuid, created, valid) VALUES(?, ?, ?, ?);")
+        ;; s-parameterized-set (prepare  "select * from users where emails=?;")
+        ;; s-parameterized-nil (prepare  "select * from users where session_token=?;")
         ]
-    (is (= user-data-set-as-map (execute->map *session* (bind s-simple))))
+    (is (= user-data-set-as-map (execute->map (bind s-simple))))
     (is (= [(first user-data-set-as-map)]
-           (execute->map *session* (bind s-parameterized-simple "mpenet"))))
+           (execute->map (bind s-parameterized-simple "mpenet"))))
     (is (= [(first user-data-set-as-map)]
-           (execute->map *session* (bind s-parameterized-simple :mpenet))))
+           (execute->map (bind s-parameterized-simple :mpenet))))
     (is (= [(first user-data-set-as-map)]
-           (execute->map *session* (bind s-parameterized-bigint 0))))
-    (is (= [] (execute *session* (bind s-prepare-types
+           (execute->map (bind s-parameterized-bigint 0))))
+    (is (= [] (execute (bind s-prepare-types
                                        "foobar"
                                        #uuid "b474e171-7757-449a-87be-d2797d1336e3"
                                        (java.util.Date.)
                                        false))))
-    (execute *session* "delete from users where user_name = 'foobar';") ;; cleanup
+    (execute  "delete from users where user_name = 'foobar';") ;; cleanup
 
     ;; ;; index on collections not supp  orted yet
     ;; (is (= [(first user-data-set)]
-    ;;        (execute *session* (bind s-parameterized-set #{"m@p.com" "ma@pe.com"}))))
+    ;;        (execute  (bind s-parameterized-set #{"m@p.com" "ma@pe.com"}))))
 
-    ;; (is (= user-data-set (execute *session* (bind s-parameterized-nil nil))))
+    ;; (is (= user-data-set (execute (bind s-parameterized-nil nil))))
     ;; 'null' parameters are not allowed since CQL3 does not (yet) supports them (see https://issues.apache.org/jira/browse/CASSANDRA-3783)
 ))
 
