e28fe33c8a2f5029c50da038999b49ccb489d43f
Max Penet <m@qbits.cc>
2013-05-08 16:08:41 +0200

    better names + changed direction (cache + prepared makes little sense, since you will pay the cost of for query generation on the client side to get values anyway, better prepare manually)

--- src/qbits/alia/hayt.clj
+++ src/qbits/alia/hayt.clj
@@ -5,23 +5,21 @@
             [qbits.alia.utils :as utils]))
 
 
-(defmulti query-strategy (fn [k & _]k))
+(defmulti query-strategy (fn [k & _] k))
+
+;; always compile
+(defmethod query-strategy :default
+  [q & _]
+  (hayt/->raw q))
 
 ;; cache query in LU
 (defmethod query-strategy :last-used
   [_ & [size]]
   (memo/memo-lu hayt/->raw size))
 
-;; always compile
-(defmethod query-strategy :default
-  [& _]
-  hayt/->raw)
-
-;;
-
 (def ^:dynamic *query* (query-strategy :last-used 100))
 
-(def set-query-fn!
+(def set-query-strategy!
   "Sets root value of *query-cach-fn*, allowing to change
    the cache factory, defaults to LU with a threshold of 100"
   (utils/var-root-setter *query*))
