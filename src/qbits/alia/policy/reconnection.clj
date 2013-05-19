e736fc2f789b415e8cdb614831824a4add3b1bef
Max Penet <m@qbits.cc>
2013-05-19 20:13:21 +0200

    docstrings on reconnection policies

--- src/qbits/alia/policy/reconnection.clj
+++ src/qbits/alia/policy/reconnection.clj
@@ -1,13 +1,20 @@
 (ns qbits.alia.policy.reconnection
+  "Policy that decides how often the reconnection to a dead node is
+attempted."
   (:import
    (com.datastax.driver.core.policies
     ConstantReconnectionPolicy
     ExponentialReconnectionPolicy)))
 
 (defn constant
+  "A reconnection policy that waits a constant time between each reconnection
+attempt."
   [constant-delay-ms]
   (ConstantReconnectionPolicy. constant-delay-ms))
 
 (defn exponential
+  "A reconnection policy that waits exponentially longer between each
+reconnection attempt (but keeps a constant delay once a maximum delay is
+reached)."
   [base-delay-ms max-delay-ms]
   (ExponentialReconnectionPolicy. base-delay-ms max-delay-ms))
