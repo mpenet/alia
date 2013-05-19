c36bf3f4138c3cd3fc8952e711f62fd41f1b6374
Max Penet <m@qbits.cc>
2013-05-19 20:02:24 +0200

    add reconnection policies

--- src/qbits/alia/policy/reconnection.clj
+++ src/qbits/alia/policy/reconnection.clj
@@ -1,1 +1,13 @@
-(ns qbits.alia.policy.reconnection)
+(ns qbits.alia.policy.reconnection
+  (:import
+   (com.datastax.driver.core.policies
+    ConstantReconnectionPolicy
+    ExponentialReconnectionPolicy)))
+
+(defn constant
+  [constant-delay-ms]
+  (ConstantReconnectionPolicy. constant-delay-ms))
+
+(defn exponential
+  [base-delay-ms max-delay-ms]
+  (ExponentialReconnectionPolicy. base-delay-ms max-delay-ms))
