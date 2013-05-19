4772b05c11d78d7c9ec40ee1af7770c72df21ab1
Max Penet <m@qbits.cc>
2013-05-19 20:08:56 +0200

    thin wrapper over load-balancing policies

--- src/qbits/alia/policy/load_balancing.clj
+++ src/qbits/alia/policy/load_balancing.clj
@@ -1,1 +1,20 @@
-(ns qbits.alia.policy.load-balancing)
+(ns qbits.alia.policy.load-balancing
+  (:import
+   (com.datastax.driver.core.policies
+    DCAwareRoundRobinPolicy
+    RoundRobinPolicy
+    TokenAwarePolicy)))
+
+(defn round-robin
+  []
+  (RoundRobinPolicy.))
+
+(defn token-aware
+  [child]
+  (TokenAwarePolicy. child))
+
+(defn dc-aware-round-robin
+  ([dc used-hosts-per-remote-dc]
+     (DCAwareRoundRobinPolicy. dc used-hosts-per-remote-dc))
+  ([dc]
+     (DCAwareRoundRobinPolicy. dc)))
