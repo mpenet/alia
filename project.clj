79aea33dd715e5b9341829d55563a7ec68320f1e
Max Penet <m@qbits.cc>
2013-08-25 13:23:12 +0200

    bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cc.qbits/alia "1.7.1"
+(defproject cc.qbits/alia "1.8.0"
   :description "Cassandra CQL3 client for Clojure - datastax/java-driver wrapper"
   :url "https://github.com/mpenet/alia"
   :license {:name "Eclipse Public License"
