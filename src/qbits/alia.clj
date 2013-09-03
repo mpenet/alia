18ed939c76f82c695d302c030ad8b73c6fd6a9e1
Max Penet <m@qbits.cc>
2013-09-03 16:19:01 +0200

    >!! blocks, fill the channel asynchronously with put!

--- src/qbits/alia.clj
+++ src/qbits/alia.clj
@@ -240,10 +240,10 @@ The query can be a raw string, a PreparedStatement (returned by
        rs-future
        (reify FutureCallback
          (onSuccess [_ result]
-           (async/>!! ch (codec/result-set->maps (.get rs-future) keywordize?))
+           (async/put! ch (codec/result-set->maps (.get rs-future) keywordize?))
            (async/close! ch))
          (onFailure [_ err]
-           (async/>!! ch err)
+           (async/put! ch err)
            (async/close! ch)))
        executor)
       ch)))
