f3734a414c60eb74d4e0cfc1c752f9b27eb71432
Michael Pershyn <michael.pershyn@adition.com>
2015-05-28 13:00:25 +0200

    added lz4 compression to docs and dev profile

--- project.clj
+++ project.clj
@@ -16,6 +16,7 @@
              :1.6  {:dependencies [[org.clojure/clojure "1.6.0"]]}
              :1.7  {:dependencies [[org.clojure/clojure "1.7.0-SNAPSHOT"]]}
              :dev  {:dependencies [[org.xerial.snappy/snappy-java "1.0.5"]
+                                   [net.jpountz.lz4/lz4 "1.2.0"]
                                    [clj-time "0.8.0"]
                                    [com.taoensso/nippy "2.8.0"]
                                    [cc.qbits/tardis "1.0.0"]
