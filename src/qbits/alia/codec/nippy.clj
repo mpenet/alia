52f748e64094fe281d7df36d22c5890e17c856c8
Max Penet <m@qbits.cc>
2015-06-27 01:13:04 +0200

    fix bytebuffer/byte-arrays mixups

--- src/qbits/alia/codec/nippy.clj
+++ src/qbits/alia/codec/nippy.clj
@@ -4,19 +4,22 @@
   manually. It only works for prepared statements (write time)"
   (:require
    [qbits.alia.codec :as codec]
-   [taoensso.nippy :as nippy]))
+   [taoensso.nippy :as nippy])
+  (:import
+   (com.datastax.driver.core.utils Bytes)
+   (java.nio ByteBuffer)))
 
 (defn set-nippy-collection-encoder!
   "Forces the encoding of *all* IPersistentCollections (non nil) via
   nippy, this might break prepared statements with cassandra
   collections. For more fine grained control you can use serializable!
   and it's encoder"
-  ([] (set-nippy-collection-encoder! nil))
+  ([] (set-nippy-collection-encoder! {}))
   ([opts]
    (extend-protocol codec/PCodec
      clojure.lang.IPersistentCollection
      (encode [coll]
-       (nippy/freeze coll opts)))))
+       (ByteBuffer/wrap (nippy/freeze coll opts))))))
 
 (defn set-nippy-decoder!
   "Forces the decoding of ByteBuffers returned by cassandra via nippy
@@ -28,7 +31,7 @@
      java.nio.ByteBuffer
      (encode [x] x)
      (decode [bb]
-       (nippy/thaw bb opts)))))
+       (nippy/thaw (Bytes/getArray bb) opts)))))
 
 (deftype NippySerializable [data])
 
@@ -37,17 +40,17 @@
 Encoding is manual, and only applied to values that are marked by
 calling serializable! on them. Takes a map or nippy options to be passed
   to thaw/freeze."
-  ([] (set-nippy-serializable-encoder! nil))
+  ([] (set-nippy-serializable-encoder! {}))
   ([opts]
    (extend-protocol codec/PCodec
      java.nio.ByteBuffer
      (encode [x] x)
      (decode [bb]
-       (nippy/thaw bb opts))
+       (nippy/thaw (Bytes/getArray bb) opts))
 
-     NippySerializable
+     qbits.alia.codec.nippy.NippySerializable
      (encode [x]
-       (nippy/freeze (.data x) opts)))))
+       (ByteBuffer/wrap (nippy/freeze (.data x) opts))))))
 
 (defn serializable!
   "Mark value as nippy serializable"
