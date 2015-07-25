dfe44d25706d5c92f3b711c6c6cd88849f85edc9
Max Penet <m@qbits.cc>
2015-07-25 11:53:37 +0200

    remove useless stuff since we defer to travis for testing

--- project.clj
+++ project.clj
@@ -27,9 +27,7 @@
                                    [cc.qbits/tardis "1.0.0"]
                                    [codox "0.8.10"]
                                    [manifold "0.1.0-beta3"]
-                                   [org.slf4j/slf4j-log4j12 "1.7.3"]]
-                    :resource-paths ["test/resources"]
-                    :jvm-opts     ["-javaagent:lib/jamm-0.2.5.jar"]}}
+                                   [org.slf4j/slf4j-log4j12 "1.7.3"]]}}
   :jar-exclusions [#"log4j.properties"]
   :codox {:src-dir-uri "https://github.com/mpenet/alia/blob/master/"
           :src-linenum-anchor-prefix "L"
