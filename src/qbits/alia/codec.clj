089664cd28d50963d148d848c7f1285e90704691
Max Penet <m@qbits.cc>
2015-09-30 19:12:02 +0200

    kill jmp table for DataType enum since java-driver now allows getObject on row cells

--- src/qbits/alia/codec.clj
+++ src/qbits/alia/codec.clj
@@ -1,12 +1,10 @@
 (ns qbits.alia.codec
-  (:require [qbits.commons :refer [case-enum]])
   (:import
    (java.nio ByteBuffer)
    (com.datastax.driver.core
     DataType
     DataType$Name
     GettableByIndexData
-    KeyspaceMetadata
     ResultSet
     Row
     UserType$Field
@@ -24,59 +22,10 @@
     "Encodes clj value into a valid cassandra value for prepared
     statements (usefull for external libs such as joda time)"))
 
-(declare deserialize decode-xform)
+(declare decode-xform)
 
-(defmacro defdeserializers [[x idx col-type] & specs]
-  (let [args [(vary-meta x assoc :tag "com.datastax.driver.core.GettableByIndexData")
-              (vary-meta idx assoc :tag "java.lang.Integer")
-              (vary-meta col-type assoc :tag "com.datastax.driver.core.DataType")]]
-    `(do
-       ~@(mapv (fn [[decoder-type form]]
-                 `(defn ~(->> decoder-type name (str "decode-") symbol)
-                    ~args
-                    ~form))
-               (partition 2 specs))
-       (defn deserialize ~args
-         (case-enum (-> ~col-type .getName)
-                    ~@(mapcat (fn [[decoder-type _]]
-                                [(->> (name decoder-type)
-                                      (.toUpperCase)
-                                      (str "com.datastax.driver.core.DataType$Name/")
-                                      symbol)
-                                 `(~(symbol (str "decode-" (name decoder-type)))
-                                   ~x ~idx ~col-type)])
-                              (partition 2 specs)))))))
-
-(defn ^Class types-args->type
-  [^"[Lcom.datastax.driver.core.DataType;" type-args pred]
-  (.asJavaClass ^DataType (pred type-args)))
-
-(defdeserializers [x idx col-type]
-  :ascii     (decode (.getString x idx))
-  :bigint    (decode (.getLong x idx))
-  :blob      (decode (.getBytes x idx))
-  :boolean   (decode (.getBool x idx))
-  :counter   (decode (.getLong x idx))
-  :custom    (decode (.getBytesUnsafe x idx))
-  :decimal   (decode (.getDecimal x idx))
-  :double    (decode (.getDouble x idx))
-  :float     (decode (.getFloat x idx))
-  :inet      (decode (.getInet x idx))
-  :int       (decode (.getInt x idx))
-  :text      (decode (.getString x idx))
-  :timestamp (decode (.getDate x idx))
-  :timeuuid  (decode (.getUUID x idx))
-  :uuid      (decode (.getUUID x idx))
-  :varchar   (decode (.getString x idx))
-  :varint    (decode (.getVarint x idx))
-  :list      (decode (.getList x idx (types-args->type (.getTypeArguments col-type) first)))
-  :set       (decode (.getSet x idx (types-args->type (.getTypeArguments col-type) first)))
-  :map       (let [t (.getTypeArguments col-type)]
-               (decode (.getMap x idx
-                                (types-args->type t first)
-                                (types-args->type t second))))
-  :tuple     (some-> x (.getTupleValue idx) decode)
-  :udt       (some-> x (.getUDTValue idx) decode))
+(defn deserialize [^GettableByIndexData x idx]
+  (decode (.getObject x idx)))
 
 (extend-protocol PCodec
 
@@ -116,24 +65,19 @@
           (let [^UserType$Field type (.next udt-type-iter)]
             (recur (assoc! udt
                            (-> type .getName keyword)
-                           (decode (deserialize udt-value
-                                                idx'
-                                                (.getType type))))
+                           (deserialize udt-value idx'))
                    (unchecked-inc-int idx')))))))
 
   TupleValue
   (encode [x] x)
   (decode [tuple-value]
-    (let [types (.getComponentTypes (.getType tuple-value))
-          len (.size types)]
+    (let [len (.size (.getComponentTypes (.getType tuple-value)))]
       (loop [tuple (transient [])
              idx' 0]
         (if (= idx' len)
           (persistent! tuple)
           (recur (conj! tuple
-                        (decode (deserialize tuple-value
-                                             idx'
-                                             (.get types idx'))))
+                        (deserialize tuple-value idx'))
                  (unchecked-inc-int idx'))))))
 
   Object
@@ -169,7 +113,7 @@
         (recur (unchecked-inc-int idx)
                (assoc! row-map
                        (key-fn (.getName cdef idx))
-                       (deserialize row idx (.getType cdef idx))))))))
+                       (deserialize row idx)))))))
 
 (defn ->result-set
   [^ResultSet result-set key-fn]
