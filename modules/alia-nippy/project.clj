6c4cc9a34d3ec6eeadbbaee8424386512831a790
Max Penet <m@qbits.cc>
2016-05-03 08:08:46 +0200

    bump

--- modules/alia-nippy/project.clj
+++ modules/alia-nippy/project.clj
@@ -1,4 +1,4 @@
-(defproject cc.qbits/alia-nippy "3.1.3"
+(defproject cc.qbits/alia-nippy "3.1.4"
   :description "Alia extension for joda-time codec"
   :url "https://github.com/mpenet/alia/alia-joda-time"
   :license {:name "Eclipse Public License"
