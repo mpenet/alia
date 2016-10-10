2bc26adb6a9a565d0125bcd1474e983f24d3427a
Max Penet <m@qbits.cc>
2016-10-10 09:03:56 +0200

    bump dependencies (c.async, manifold, driver) and release

--- modules/alia-async/project.clj
+++ modules/alia-async/project.clj
@@ -1,9 +1,9 @@
-(defproject cc.qbits/alia-async "3.1.3"
+(defproject cc.qbits/alia-async "3.1.4"
   :description "core.async interface for Alia"
   :url "https://github.com/mpenet/alia"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [cc.qbits/alia "3.1.3"]
-                 [org.clojure/core.async "0.2.374"]]
+                 [cc.qbits/alia "3.1.11"]
+                 [org.clojure/core.async "0.2.391"]]
   :global-vars {*warn-on-reflection* true})
--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
-(defproject cc.qbits/alia-all "3.2.0"
+(defproject cc.qbits/alia-all "3.2.1"
   :description "Cassandra CQL3 client for Clojure - datastax/java-driver wrapper"
   :url "https://github.com/mpenet/alia"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0-alpha12"]
-                 [cc.qbits/alia "3.1.10"]
-                 [cc.qbits/alia-manifold "3.1.4"]
-                 [cc.qbits/alia-async "3.1.3"]
+                 [cc.qbits/alia "3.1.11"]
+                 [cc.qbits/alia-manifold "3.1.5"]
+                 [cc.qbits/alia-async "3.1.4"]
                  [cc.qbits/alia-joda-time "3.1.3"]
                  [cc.qbits/alia-nippy "3.1.4"]
                  [cc.qbits/alia-eaio-uuid "3.1.3"]
@@ -18,14 +18,15 @@
                                    [com.taoensso/nippy "2.9.0"]
                                    [cc.qbits/tardis "1.0.0"]
                                    [codox "0.9.1"]
-                                   [manifold "0.1.2"]
+                                   [manifold "0.1.5"]
                                    [org.slf4j/slf4j-log4j12 "1.7.3"]]}}
   :sub ["modules/alia"
         "modules/alia-manifold"
         "modules/alia-async"
         "modules/alia-joda-time"
         "modules/alia-nippy"
-        "modules/alia-eaio-uuid"]
+        "modules/alia-eaio-uuid"
+        "modules/alia-spec"]
   :jar-exclusions [#"log4j.properties"]
   :codox {:source-uri "https://github.com/mpenet/alia/blob/master/{filepath}#L{line}"
           :metadata {:doc/format :markdown}
