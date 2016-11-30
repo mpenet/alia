89cf823cf8133541312a4e3c97afa9a4c8086ac8
Max Penet <m@qbits.cc>
2016-11-30 17:22:15 +0100

    bump all the things

--- modules/alia-spec/project.clj
+++ modules/alia-spec/project.clj
@@ -3,8 +3,8 @@
   :url "https://github.com/mpenet/alia/alia-joda-time"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha12"]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha14"]
                  [org.clojure/test.check "0.9.0"]
                  [cc.qbits/spex "0.1.1"]
-                 [cc.qbits/alia "3.2.0" :exclude [org.clojure/clojure]]]
+                 [cc.qbits/alia "4.0.0-beta1" :exclude [org.clojure/clojure]]]
   :global-vars {*warn-on-reflection* true})
