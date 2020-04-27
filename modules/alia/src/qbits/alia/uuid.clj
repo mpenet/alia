f24c4e9197f822386868e74aa06b814e795f638a
mccraig mccraig of the clan mccraig <mccraigmccraig@gmail.com>
2020-04-27 17:36:14 +0100

    alia/uuid

--- modules/alia/src/qbits/alia/uuid.clj
+++ modules/alia/src/qbits/alia/uuid.clj
@@ -1,29 +1,29 @@
 (ns qbits.alia.uuid
-  (:import (com.datastax.driver.core.utils UUIDs)))
+  (:import (com.datastax.oss.driver.api.core.uuid Uuids)))
 
 (defn time-based
   "Creates a new time-based (version 1) UUID."
   []
-  (UUIDs/timeBased))
+  (Uuids/timeBased))
 
 (defn start-of
   "Creates a 'fake' time-based UUID that sorts as the smallest
   possible version 1 UUID generated at the provided timestamp."
   [ts]
-  (UUIDs/startOf (long ts)))
+  (Uuids/startOf (long ts)))
 
 (defn end-of
   "Creates a 'fake' time-based UUID that sorts as the biggest possible
   version 1 UUID generated at the provided timestamp."
   [ts]
-  (UUIDs/endOf (long ts)))
+  (Uuids/endOf (long ts)))
 
 (defn unix-timestamp
   "Return the unix timestamp contained by the provided time-based UUID."
   [uuid]
-  (UUIDs/unixTimestamp uuid))
+  (Uuids/unixTimestamp uuid))
 
 (defn random
   "Creates a new random (version 4) UUID."
   []
-  (UUIDs/random))
+  (Uuids/random))
