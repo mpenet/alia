2774bdc96fe718a0b2e25cc7e96db6b19906525a
mccraig mccraig of the clan mccraig <mccraigmccraig@gmail.com>
2020-04-27 18:16:28 +0100

    alia/tuple

--- modules/alia/src/qbits/alia/tuple.clj
+++ modules/alia/src/qbits/alia/tuple.clj
@@ -1,43 +1,52 @@
 (ns qbits.alia.tuple
   (:import
-   (com.datastax.driver.core
-    Session
-    TupleValue
-    UDTValue
-    TupleType)
-   (java.util
-    UUID
-    List
-    Map
-    Set
-    Date)
-   (java.net InetAddress)
-   (java.nio ByteBuffer)))
+   [com.datastax.oss.driver.api.core.session Session]
+   [com.datastax.oss.driver.api.core.type DataType TupleType]
+   [com.datastax.oss.driver.api.core.data TupleValue UdtValue]
+   [com.datastax.oss.driver.api.core.metadata.schema
+    KeyspaceMetadata
+    TableMetadata
+    ColumnMetadata]
+   [java.util UUID List Map Set]
+   [java.time Instant LocalDate LocalTime]
+   [java.net InetAddress]
+   [java.nio ByteBuffer]))
 
 (defprotocol Encoder
-  (-set-field! [x u i]))
+  (-set-field!
+    [x u i]
+    [x u i ec]
+    [x u i kc vc]))
 
 (extend-protocol Encoder
 
   BigInteger
   (-set-field! [b u i]
-    (.setVarint ^TupleValue u i b))
+    (.setBigInteger ^TupleValue u i b))
 
   Boolean
   (-set-field! [b u i]
-    (.setBool ^TupleValue u i b))
+    (.setBoolean ^TupleValue u i b))
 
   ByteBuffer
   (-set-field! [b u i]
-    (.setBytes ^TupleValue u i b))
+    (.setByteBuffer ^TupleValue u i b))
 
-  Date
+  LocalDate
   (-set-field! [d u i]
-    (.setDate ^TupleValue u i d))
+    (.setLocalDate ^TupleValue u i d))
+
+  LocalTime
+  (-set-field! [t u i]
+    (.setLocalTime ^TupleValue u i t))
+
+  Instant
+  (-set-field! [inst u i]
+    (.setInstant ^TupleValue u i inst))
 
   BigDecimal
   (-set-field! [d u i]
-    (.setDecimal ^TupleValue u i d))
+    (.setBigDecimal ^TupleValue u i d))
 
   Double
   (-set-field! [d u i]
@@ -49,27 +58,27 @@
 
   InetAddress
   (-set-field! [x u i]
-    (.setInet ^TupleValue u i x))
+    (.setInetAddress ^TupleValue u i x))
 
   Integer
   (-set-field! [x u i]
     (.setInt ^TupleValue u i x))
 
   List
-  (-set-field! [l u i]
-    (.setList ^TupleValue u i l))
+  (-set-field! [l u i ec]
+    (.setList ^TupleValue u i l ^Class ec))
 
   Long
   (-set-field! [l u i]
     (.setLong ^TupleValue u i l))
 
   Map
-  (-set-field! [m u i]
-    (.setMap ^TupleValue u i m))
+  (-set-field! [m u i kc vc]
+    (.setMap ^TupleValue u i m ^Class kc ^Class vc))
 
   Set
-  (-set-field! [s u i]
-    (.setSet ^TupleValue u i s))
+  (-set-field! [s u i ec]
+    (.setSet ^TupleValue u i s ^Class ec))
 
   String
   (-set-field! [x u i]
@@ -87,33 +96,56 @@
   (-set-field! [t u i]
     (.setTupleValue ^TupleValue u i t))
 
-  UDTValue
+  UdtValue
   (-set-field! [x u i]
-    (.setUDTValue ^TupleValue u i x))
+    (.setUdtValue ^TupleValue u i x))
 
   UUID
   (-set-field! [uuid u i]
-    (.setUUID ^TupleValue u i uuid)))
+    (.setUuid ^TupleValue u i uuid)))
 
 (defn set-field!
   "Where's flip when you need it"
-  [u k x]
-  (-set-field! x u k))
+  ([u k x]
+   (-set-field! x u k))
+  ([u k x ec]
+   (-set-field! x u k ec))
+  ([u k x kc vc]
+   (-set-field! x u k kc vc)))
 
 (defn encoder
   "Takes a Session, optionaly keyspace name, table name and column
   name and returns a function that can be used to encode a collection into
-  a TupleValue suitable to be used in PreparedStatements"
+  a TupleValue suitable to be used in PreparedStatements
+
+  TODO broken for collection values - java-driver-4 changed the API"
   ([^Session session table column codec]
-   (encoder session (.getLoggedKeyspace session) table column codec))
+   (encoder session (.getKeyspace session) table column codec))
   ([^Session session ks table column codec]
-   (let [^TupleType t (some-> session
-                              .getCluster
-                              .getMetadata
-                              (.getKeyspace (name ks))
-                              (.getTable (name table))
-                              (.getColumn (name column))
-                              (.getType))
+   (let [^KeyspaceMetadata ks-metadata
+         (some-> session
+                 .getMetadata
+                 (.getKeyspace (name (or ks (.getKeyspace session))))
+                 (.get))
+
+         ^TableMetadata table-metadata
+         (some-> ks-metadata
+                 (.getTable (name table))
+                 (.get))
+
+         ^ColumnMetadata column-metadata
+         (some-> table-metadata
+                 (.getColumn (name column))
+                 (.get))
+
+
+         ^DataType dt
+         (some-> column-metadata
+                 (.getType))
+
+         ^TupleType t (when (instance? TupleType dt)
+                        dt)
+
          encode (:encoder codec)]
      (when-not t
        (throw (ex-info (format "Tuple Column '%s' not found on Keyspace '%s'"
