b89bd3c979d923f84aec6eb6428a7eff89738002
mccraig mccraig of the clan mccraig <mccraigmccraig@gmail.com>
2020-12-22 10:52:57 +0000

    unwrap j.u.c exceptions

--- modules/alia/src/qbits/alia/completable_future.clj
+++ modules/alia/src/qbits/alia/completable_future.clj
@@ -1,6 +1,11 @@
 (ns qbits.alia.completable-future
   (:import
-   [java.util.concurrent Executor CompletionStage CompletableFuture]
+   [java.util.concurrent
+    Executor
+    CompletionStage
+    CompletableFuture
+    ExecutionException
+    CompletionException]
    [java.util.function BiFunction]))
 
 (defn completed-future
@@ -14,19 +19,31 @@
     (.completeExceptionally f e)
     f))
 
+(defn ex-unwrap
+  "Unwraps exceptions if we have a valid ex-cause present"
+  [ex]
+  (if (or (instance? ExecutionException ex)
+          (instance? CompletionException ex))
+    (or (ex-cause ex) ex)
+    ex))
+
 (defn handle-completion-stage
   "java incantation to handle both branches of a completion-stage"
-  [^CompletionStage completion-stage
-   on-success
-   on-error
-   {executor :executor
-    :as opts}]
-  (let [handler-bifn (reify BiFunction
-                       (apply [_ r ex]
-                         (if (some? ex)
-                           (on-error ex)
-                           (on-success r))))]
+  ([^CompletionStage completion-stage
+    on-success
+    on-error
+    {executor :executor
+     :as opts}]
+   (let [handler-bifn (reify BiFunction
+                        (apply [_ r ex]
+                          (if (some? ex)
+                            (on-error (ex-unwrap ex))
+                            (on-success r))))]
 
-    (if (some? executor)
-      (.handleAsync completion-stage handler-bifn executor)
-      (.handleAsync completion-stage handler-bifn))))
+     (if (some? executor)
+       (.handleAsync completion-stage handler-bifn executor)
+       (.handleAsync completion-stage handler-bifn))))
+  ([^CompletionStage completion-stage
+    on-success
+    on-error]
+   (handle-completion-stage completion-stage on-success on-error {})))
