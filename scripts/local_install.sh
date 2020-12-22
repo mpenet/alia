d7a9325a398df46479cf4fddc9164094912429fe
mccraig mccraig of the clan mccraig <mccraigmccraig@gmail.com>
2020-12-22 14:34:53 +0000

    alia-component is dependent on alia-spec

--- scripts/local_install.sh
+++ scripts/local_install.sh
@@ -6,8 +6,6 @@ cd modules/alia
 lein install
 cd ../../modules/alia-async
 lein install
-cd ../../modules/alia-component
-lein install
 cd ../../modules/alia-java-legacy-time
 lein install
 cd ../../modules/alia-joda-time
@@ -15,4 +13,6 @@ lein install
 cd ../../modules/alia-manifold
 lein install
 cd ../../modules/alia-spec
+lein install
+cd ../../modules/alia-component
 lein install
