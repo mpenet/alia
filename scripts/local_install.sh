5cca39d2a66d65ce6446950996c01b6cd47552be
mccraig mccraig of the clan mccraig <mccraigmccraig@gmail.com>
2020-12-22 14:31:14 +0000

    local install script

---
+++ scripts/local_install.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." >/dev/null 2>&1 && pwd )"
+
+cd modules/alia
+lein install
+cd ../../modules/alia-async
+lein install
+cd ../../modules/alia-component
+lein install
+cd ../../modules/alia-java-legacy-time
+lein install
+cd ../../modules/alia-joda-time
+lein install
+cd ../../modules/alia-manifold
+lein install
+cd ../../modules/alia-spec
+lein install
