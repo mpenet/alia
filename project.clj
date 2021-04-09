851577ba9225e352101480b5f732b2dcbd9ef5ea
mccraig mccraig of the clan mccraig <mccraigmccraig@gmail.com>
2021-04-09 17:49:31 +0100

    Version 5.0.0-alpha5

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cc.qbits/alia-all "5.0.0-alpha5-SNAPSHOT"
+(defproject cc.qbits/alia-all "5.0.0-alpha5"
   :description "Cassandra CQL3 client for Clojure - datastax/java-driver wrapper"
   :url "https://github.com/mpenet/alia"
   :scm {:name "git"
